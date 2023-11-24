19a8b2b69aa962b40cd4c10999200f004fdd137e
NEERAJ023 <143694023+NEERAJ023@users.noreply.github.com>
2023-11-24 20:57:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
+let table=document.querySelector("#sampleTable");
 function insert_Row() {
     //Write your code here
-  
-  
+	
+  let tr= document.createElement("tr");
+	let value={
+		val1:"New Cell1",
+		val2:"New Cell2"
+	}
+	for(let key in value){
+		let td=document.createElement("td");
+		td.innerText=value[key];
+		tr.appendChild(td);
+	}
+	table.prepend(tr);
 }
